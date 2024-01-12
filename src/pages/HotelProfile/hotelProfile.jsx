597140cd87c59866e8f81af50ba03f72b77b8431
sushantyerawar1 <84599862+sushantyerawar1@users.noreply.github.com>
2024-01-12 22:50:18 +0530

    Added Change profilepic feature

--- src/pages/HotelProfile/hotelProfile.jsx
+++ src/pages/HotelProfile/hotelProfile.jsx
@@ -13,11 +13,14 @@ import {
     Heading,
     Textarea,
     Button,
-    Text,
+    Image,
+    Tooltip,
+    Text
 } from '@chakra-ui/react'
 import { useToast } from "@chakra-ui/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import profile from "../../profile.png";
 
 const HotelProfile = () => {
 
@@ -132,6 +135,68 @@ const HotelProfile = () => {
         }
     };
 
+
+    const [flag, setFlag] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(profile);
+    const [selectedNewImage, setSelectedNewImage] = useState(null);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
+            setSelectedNewImage(URL.createObjectURL(file));
+        } else {
+            toast({
+                title: "Invalid Image Format",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        }
+    };
+
+    const ToggleButton = (e) => {
+        e.preventDefault();
+        setFlag(true)
+    }
+
+
+
+    // const handleUpdateImage = (e) => {
+    //     e.preventDefault();
+    //     alert("Uploaded Successfully");
+    //     setSelectedImage(selectedNewImage);
+    //     setFlag(false);
+    // }
+    const handleUpdateImage = (e) => {
+        e.preventDefault();
+        if (selectedNewImage) {
+            toast({
+                title: "Image Uploaded Successfully",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setSelectedImage(selectedNewImage);
+            setFlag(false);
+        } else {
+            toast({
+                title: "Please select an image",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        }
+    };
+
+    const handleDeleteImage = (e) => {
+        e.preventDefault();
+        setFlag(false);
+    }
+
     return (
         <>
             <Header />
@@ -145,7 +210,7 @@ const HotelProfile = () => {
             >
                 <Stack spacing={8} mx={'auto'} width={'50%'} py={12} px={6} >
                     <Stack align={'center'}>
-                        <Heading fontSize={'60px'}>Hotel Details</Heading>
+                        <Heading fontSize={'60px'}>Profile</Heading>
                     </Stack>
                     <Box
                         rounded={'lg'}
@@ -154,7 +219,77 @@ const HotelProfile = () => {
                         boxShadow="5px 10px 18px #888888"
                         p={8}
                     >
+                        <Box
+                            w="200px"
+                            h="200px"
+                            borderRadius="full"
+                            overflow="hidden"
+                            bg="green"
+                            color="white"
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            ml={"35%"}
+                            position="relative"
+                        >
+                            <Image
+                                src={selectedImage}
+                                alt="User Image"
+                                objectFit="cover"
+                                boxSize="100%"
+                            />
+                            {!flag && (
+                                <Tooltip label="update Image" placement="bottom">
+                                    <FontAwesomeIcon
+                                        icon={faTrash}
+                                        color="black"
+                                        style={{
+                                            position: 'absolute',
+                                            bottom: "0%",
+                                            cursor: 'pointer',
+
+                                        }}
+                                        onClick={ToggleButton}
+                                    />
+                                </Tooltip>
+                            )}
+                        </Box>
+                        {flag && (
+                            <Flex width={"100%"} justify={"center"}>
+                                <Box>
+                                    {(selectedNewImage && flag) &&
+                                        <Button mt={2} mr={"10%"} onClick={handleUpdateImage} colorScheme="blue" width={"100%"} isDisabled={!selectedNewImage}>
+                                            Upload
+                                        </Button>
+                                    }
+                                    {(!selectedNewImage && flag) && (
+                                        <Tooltip label="Select an Image" placement="bottom">
+                                            <Button mt={2} mr={"10%"} onClick={handleUpdateImage} colorScheme="blue" width={"100%"} isDisabled={!selectedNewImage}>
+                                                Upload
+                                            </Button>
+                                        </Tooltip>
+                                    )}
+                                </Box>
+                                <Box>
+                                    <Button mt={2} ml={"10%"} onClick={handleDeleteImage} colorScheme="red" width={"100%"}>
+                                        Delete
+                                    </Button>
+                                </Box>
+                            </Flex>
+                        )}
                         <Stack spacing={4} align={"center"}>
+                            {
+                                flag &&
+                                <FormControl id="image" isRequired>
+                                    <FormLabel>Upload Profile Image</FormLabel>
+                                    <Input
+                                        p={1.5}
+                                        type="file"
+                                        accept=".jpg, .jpeg, .png"
+                                        onChange={handleImageChange}
+                                    />
+                                </FormControl>
+                            }
                             <FormControl id="name" isRequired>
                                 <FormLabel>Name of Hotel</FormLabel>
                                 <Input
--- src/pages/UserProfile/userProfile.jsx
+++ src/pages/UserProfile/userProfile.jsx
@@ -13,11 +13,15 @@ import {
     Heading,
     Textarea,
     Button,
-    Text,
+    Image,
+    Tooltip
 } from '@chakra-ui/react'
 import { useToast } from "@chakra-ui/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import profile from "../../profile.png";
+
+
 
 const UserProfile = () => {
 
@@ -38,6 +42,7 @@ const UserProfile = () => {
             navigate("/login")
         }
     })
+
 
     // const handleDelete = (indexToDelete) => {
     //     const updatedFiles = selectedFiles.filter((_, index) => index !== indexToDelete);
@@ -121,6 +126,68 @@ const UserProfile = () => {
         }
     };
 
+
+    const [flag, setFlag] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(profile);
+    const [selectedNewImage, setSelectedNewImage] = useState(null);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
+            setSelectedNewImage(URL.createObjectURL(file));
+        } else {
+            toast({
+                title: "Invalid Image Format",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        }
+    };
+
+    const ToggleButton = (e) => {
+        e.preventDefault();
+        setFlag(true)
+    }
+
+
+
+    // const handleUpdateImage = (e) => {
+    //     e.preventDefault();
+    //     alert("Uploaded Successfully");
+    //     setSelectedImage(selectedNewImage);
+    //     setFlag(false);
+    // }
+    const handleUpdateImage = (e) => {
+        e.preventDefault();
+        if (selectedNewImage) {
+            toast({
+                title: "Image Uploaded Successfully",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setSelectedImage(selectedNewImage);
+            setFlag(false);
+        } else {
+            toast({
+                title: "Please select an image",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        }
+    };
+
+    const handleDeleteImage = (e) => {
+        e.preventDefault();
+        setFlag(false);
+    }
+
     return (
         <>
             <Header />
@@ -134,7 +201,7 @@ const UserProfile = () => {
             >
                 <Stack spacing={8} mx={'auto'} width={'50%'} py={12} px={6} >
                     <Stack align={'center'}>
-                        <Heading fontSize={'60px'}>User Details</Heading>
+                        <Heading fontSize={'60px'}>Profile</Heading>
                     </Stack>
                     <Box
                         rounded={'lg'}
@@ -143,7 +210,78 @@ const UserProfile = () => {
                         boxShadow="5px 10px 18px #888888"
                         p={8}
                     >
+
+                        <Box
+                            w="200px"
+                            h="200px"
+                            borderRadius="full"
+                            overflow="hidden"
+                            bg="green"
+                            color="white"
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            ml={"35%"}
+                            position="relative"
+                        >
+                            <Image
+                                src={selectedImage}
+                                alt="User Image"
+                                objectFit="cover"
+                                boxSize="100%"
+                            />
+                            {!flag && (
+                                <Tooltip label="update Image" placement="bottom">
+                                    <FontAwesomeIcon
+                                        icon={faTrash}
+                                        color="black"
+                                        style={{
+                                            position: 'absolute',
+                                            bottom: "0%",
+                                            cursor: 'pointer',
+
+                                        }}
+                                        onClick={ToggleButton}
+                                    />
+                                </Tooltip>
+                            )}
+                        </Box>
+                        {flag && (
+                            <Flex width={"100%"} justify={"center"}>
+                                <Box>
+                                    {(selectedNewImage && flag) &&
+                                        <Button mt={2} mr={"10%"} onClick={handleUpdateImage} colorScheme="blue" width={"100%"} isDisabled={!selectedNewImage}>
+                                            Upload
+                                        </Button>
+                                    }
+                                    {(!selectedNewImage && flag) && (
+                                        <Tooltip label="Select an Image" placement="bottom">
+                                            <Button mt={2} mr={"10%"} onClick={handleUpdateImage} colorScheme="blue" width={"100%"} isDisabled={!selectedNewImage}>
+                                                Upload
+                                            </Button>
+                                        </Tooltip>
+                                    )}
+                                </Box>
+                                <Box>
+                                    <Button mt={2} ml={"10%"} onClick={handleDeleteImage} colorScheme="red" width={"100%"}>
+                                        Delete
+                                    </Button>
+                                </Box>
+                            </Flex>
+                        )}
                         <Stack spacing={4} align={"center"}>
+                            {
+                                flag &&
+                                <FormControl id="image" isRequired>
+                                    <FormLabel>Upload Profile Image</FormLabel>
+                                    <Input
+                                        p={1.5}
+                                        type="file"
+                                        accept=".jpg, .jpeg, .png"
+                                        onChange={handleImageChange}
+                                    />
+                                </FormControl>
+                            }
                             <FormControl id="name" isRequired>
                                 <FormLabel>Name of User</FormLabel>
                                 <Input
@@ -203,8 +341,7 @@ const UserProfile = () => {
                         </Stack>
                     </Box>
                 </Stack>
-            </Flex>
-
+            </Flex >
             <Footer />
         </>
     )
